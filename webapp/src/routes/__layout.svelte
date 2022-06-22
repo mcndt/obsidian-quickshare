8f07dd5965c1d61843a10a455c1a20d723ccdc18
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-22 18:01:08 +0200

    add stuff

--- webapp/src/routes/__layout.svelte
+++ webapp/src/routes/__layout.svelte
@@ -4,7 +4,23 @@
 	import '../app.css';
 </script>
 
-<div class="container mx-auto max-w-2xl mx-auto mt-6 px-4">
+<div id="navbar" class="h-14 border-b border-neutral-200">
+	<div
+		id="navbar-content"
+		class="h-full max-w-6xl mx-auto flex items-center justify-between content-center"
+	>
+		<div id="navbar-left" class="flex gap-8 flex">
+			<span id="name" class="font-extrabold text-lg">[Obsidian Note Sharing]</span>
+			<ul class="flex gap-5 content-center">
+				<li class="hidden lg:block self-center">Install</li>
+				<li class="hidden lg:block self-center">About</li>
+			</ul>
+		</div>
+		<div id="navbar-right">test</div>
+	</div>
+</div>
+
+<div class="container mx-auto max-w-2xl mx-auto mt-6 px-4 2xl:px-0">
 	<slot />
 	<Footer />
 </div>
--- webapp/tailwind.config.cjs
+++ webapp/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
-	},
-	plugins: []
+	}
 };

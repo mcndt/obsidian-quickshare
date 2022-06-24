2d54180a2c70e4af8b3c7fe83df06195f844ed94
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-24 18:31:11 +0200

    detect callouts

--- webapp/src/lib/marked/renderers/Blockquote.svelte
+++ webapp/src/lib/marked/renderers/Blockquote.svelte
@@ -1,3 +1,13 @@
-<p class="border border-l-4 ml-4 prose-p:my-2 prose-p:mx-4">
-	<slot />
-</p>
+<script lang="ts">
+	export let raw: string;
+
+	let isCallout = raw.split('\n')[0].match(/>\s?\[!(.+)\]\s(.*) /) != null;
+</script>
+
+{#if isCallout}
+	<p>IS CALLOUT!!</p>
+{:else}
+	<p class="border border-l-4 ml-4 prose-p:my-2 prose-p:mx-4">
+		<slot />
+	</p>
+{/if}

12d79cdfbad7acc4cc1e72a675a8d75e595ab06f
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-26 21:58:04 +0200

    make checkboxes unclickable

--- webapp/src/lib/marked/renderers/ListItem.svelte
+++ webapp/src/lib/marked/renderers/ListItem.svelte
@@ -10,7 +10,7 @@
 				type="checkbox"
 				bind:checked
 				value=""
-				class="w-4 h-4 accent-[#7b6cd9] text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				class="pointer-events-none w-4 h-4 accent-[#7b6cd9] text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
 			/>
 			<!-- <label for="default-checkbox" class="ml-2 text-sm text-gray-900 dark:text-gray-300"
 				><slot /></label

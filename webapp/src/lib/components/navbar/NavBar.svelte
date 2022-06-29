2e98fa7f2d0d0bf82cfb7aaed5be9cfc4deba4fb
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-30 01:01:55 +0200

    fix title centering

--- webapp/src/lib/components/navbar/NavBar.svelte
+++ webapp/src/lib/components/navbar/NavBar.svelte
@@ -3,9 +3,9 @@
 		id="navbar-content"
 		class="h-full px-4 6xl:px-0 max-w-6xl mx-auto flex items-center justify-between content-center whitespace-nowrap"
 	>
-		<div id="navbar-left" class="flex gap-8 flex">
-			<a href="/">
-				<span id="name" class="font-extrabold text-lg dark:text-white"
+		<div id="navbar-left" class="flex gap-8">
+			<a href="/" class="self-center h-full pb-0.5">
+				<span id="name" class="self-center font-extrabold text-lg dark:text-white"
 					>[[Obsidian Note Sharing]]</span
 				>
 			</a>

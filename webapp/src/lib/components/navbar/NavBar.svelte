dd235e4c614bdd48ad1583d9ca4efc6ad22b8ac3
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-30 21:37:59 +0200

    branding

--- webapp/src/lib/components/navbar/NavBar.svelte
+++ webapp/src/lib/components/navbar/NavBar.svelte
@@ -1,15 +1,13 @@
-<div id="navbar" class="h-14 border-b border-zinc-200 dark:border-neutral-700 transition-colors">
+<div id="navbar" class="h-14 border-b border-zinc-200 dark:border-zinc-700 transition-colors">
 	<div
 		id="navbar-content"
 		class="h-full px-4 6xl:px-0 max-w-6xl mx-auto flex items-center justify-between content-center whitespace-nowrap"
 	>
-		<div id="navbar-left" class="flex gap-8">
+		<div id="navbar-left" class="flex gap-6">
 			<a href="/" class="self-center h-full pb-0.5">
-				<span id="name" class="self-center font-extrabold text-lg dark:text-white"
-					>[[Obsidian Note Sharing]]</span
-				>
+				<span id="name" class="self-center font-bold text-lg dark:text-white">Noteshare.space</span>
 			</a>
-			<ul class="flex gap-5 content-center">
+			<ul class="flex gap-2 content-center">
 				<slot name="left" />
 			</ul>
 		</div>

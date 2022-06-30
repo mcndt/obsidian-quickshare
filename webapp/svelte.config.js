2a612bf0ee9e402ef8a01edf3c09b559281abf42
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-06-30 22:08:52 +0200

    svelte dockerfile

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */

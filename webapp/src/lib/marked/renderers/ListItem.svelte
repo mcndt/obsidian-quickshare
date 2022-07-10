1e21a1045c880f1e7a1bf891861ee2a41ce520bf
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-07-11 01:03:14 +0200

    checklist rendering

--- webapp/src/lib/marked/renderers/ListItem.svelte
+++ webapp/src/lib/marked/renderers/ListItem.svelte
@@ -5,13 +5,15 @@
 
 {#if task || checked}
 	<li class="list-none translate-x-[-1.625em] leading-relaxed">
-		<div class="inline-flex align-start justify-start items-start items-center gap-2 ">
-			<input
-				type="checkbox"
-				bind:checked
-				value=""
-				class="inline-block self-start mt-2 pointer-events-none w-4 h-4 accent-[#7b6cd9] text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-			/>
+		<div class="inline-flex align-start justify-start items-start items-center">
+			<div class="inline-block self-start mt-0.5 mr-2.5">
+				<input
+					type="checkbox"
+					bind:checked
+					value=""
+					class="pointer-events-none w-4 h-4 accent-[#7b6cd9] text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				/>
+			</div>
 			<slot />
 		</div>
 	</li>

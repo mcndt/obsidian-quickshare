adf6999ee0fa60df0cad15f058cf8803e75cbae3
Maxime Cannoodt <Maxime.Cannoodt@UGent.be>
2022-08-19 18:27:00 +0200

    fix: :bug: Potential fix for iOS loading

--- src/lib/anonUserId.ts
+++ src/lib/anonUserId.ts
@@ -1,4 +1,4 @@
-import { crc16 as crc } from "crc";
+import crc from "crc/calculators/crc16";
 
 /**
  * @returns {string} a 16 character base16 string with 12 random characters and 4 CRC characters
@@ -11,7 +11,11 @@ export function generateId() {
 	const truncated = random.slice(0, 12).padStart(12, "0");
 
 	// compute the CRC of the random number
-	const checksum = crc(truncated).toString(16).padStart(4, "0");
+
+	// create int8array from "truncated"
+	const buffer = new TextEncoder().encode(truncated);
+
+	const checksum = crc(buffer).toString(16).padStart(4, "0");
 
 	return truncated + checksum;
 }
@@ -28,9 +32,10 @@ export function checkId(id: string): boolean {
 	// extract the random number and the checksum
 	const random = id.slice(0, 12);
 	const checksum = id.slice(12, 16);
+	const buffer = new TextEncoder().encode(random);
 
 	// compute the CRC of the random number
-	const computedChecksum = crc(random).toString(16).padStart(4, "0");
+	const computedChecksum = crc(buffer).toString(16).padStart(4, "0");
 
 	// compare the computed checksum with the one in the id
 	return computedChecksum === checksum;
